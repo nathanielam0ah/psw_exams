9a888bb22487beab338f91beeedbcf489ffc5701
nathanielam0ah <nathanielam0ah@github.com>
2019-08-04 02:03:53 +0000

    Added i/o to record input history

--- exams01/Customer.py
+++ exams01/Customer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-import datetime
+from datetime import date
 
 class Customer:
-    li = []
+    li = {}
 
     def get_CustomerInfo(self):             #recieves input of customer details.
         try:
@@ -11,11 +11,17 @@ class Customer:
             email = str(input("EMAIL: "))
             twitterUsername = str(input("TWITTER USERNAME: "))
             carNo = str(input("CAR NO: "))
-            serviceTime = datetime.datetime.now()
+            serviceTime = date.today()
         except ValueError:
             print("INVALID INPUT")
         li2 = {'NAME':customerName, 'PHONE NUMBER':phone_number, 'EMAIL':email, 'TWITTER USERNAME':twitterUsername, 'CAR NUMBER':carNo, 'TIME':serviceTime}
-        Customer.li.append(li2)
+        Customer.li = li2
+
+    def save_CustomerInfo(self):                #writes customer information to file
+        writer = open("historyLog.md", "w+")
+        writer.write(str(Customer.li))
+        writer.close()
 
 newCustomer = Customer()
 newCustomer.get_CustomerInfo()
+newCustomer.save_CustomerInfo()
