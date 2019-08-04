610fd5921237804981e6c9bb29ec7f2841fc01b1
nathanielam0ah <nathanielam0ah@github.com>
2019-08-04 00:17:38 +0000

    try and except

--- exams01/Customer.py
+++ exams01/Customer.py
@@ -10,15 +10,17 @@ class Customer:
     carNo = None
     li = []
 
-    def get_CustomerInfo(self):         #recieves input of customer details.
-
-        customerName = str(input("NAME: "))
-        phone_number = str(input("PHONE NO: "))
-        email = str(input("EMAIL: "))
-        twitterUsername = str(input("TWITTER USERNAME: "))
-        carNo = str(input("CAR NO: "))
-        li2 = [customerName, phone_number, email, twitterUsername, carNo]
-        Customer.li = li2
+    def get_CustomerInfo(self):             #recieves input of customer details.
+        try:
+            customerName = str(input("NAME: "))
+            phone_number = str(input("PHONE NO: "))
+            email = str(input("EMAIL: "))
+            twitterUsername = str(input("TWITTER USERNAME: "))
+            carNo = str(input("CAR NO: "))
+        except ValueError:
+            print("INPUT ERROR")
+        li2 = {'NAME':customerName, 'PHONE NUMBER':phone_number, 'EMAIL':email, 'TWITTER':twitterUsername, 'CAR NO':carNo}
+        Customer.li.append(li2)
         print(Customer.li)
 
 newCustomer = Customer()
