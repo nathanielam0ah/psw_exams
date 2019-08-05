94a65bf5addccc33ae3368973e5e535ad95f8fdd
nathanielam0ah <nathanielam0ah@github.com>
2019-08-05 14:24:34 +0000

    Can read HistoryLog.

--- exams01/Management.py
+++ exams01/Management.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-from ServiceLog import serviceLog #importing the customer module
 
 class ManagementLog:
-    allserviceLogs = {}
 
-    def get_serviceLog(self, serviceLog_confirm):
-        allserviceLogs.append()
+    def readfile(self):
+        readLog = eval(open('HistoryLog.md', 'r').read())
+        print(readLog)
 
-class Attendance:
-
-    def __init__(self, date, servicetime):
-        self.date = date
-        self.serviceTime = serviceTime
+RetrieveLog = ManagementLog()
+RetrieveLog.readfile()
