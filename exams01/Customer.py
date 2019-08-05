bd98e0dcc526b744daffe5fd387809eafbe83363
nathanielam0ah <nathanielam0ah@github.com>
2019-08-05 14:25:21 +0000

    changed date and time format.

--- exams01/Customer.py
+++ exams01/Customer.py
@@ -11,17 +11,24 @@ class Customer:
             email = str(input("EMAIL: "))
             twitterUsername = str(input("TWITTER USERNAME: "))
             carNo = str(input("CAR NO: "))
-            serviceDate = date.today()
-            serviceTime = datetime.now().time()
         except ValueError:
             print("INVALID INPUT")
-        li2 = {'NAME':customerName, 'PHONE NUMBER':phone_number, 'EMAIL':email, 'TWITTER USERNAME':twitterUsername, 'CAR NUMBER':carNo, 'DATE':serviceDate, 'TIME':serviceTime}
+        today = date.today()
+        now = datetime.now()
+        serviceDate = today.strftime("%d/%m/%Y")
+        serviceTime = now.strftime("%H:%M:%S")
+        li2 = {'DATE':serviceDate, 'TIME':serviceTime, 'NAME':customerName, 'PHONE NUMBER':phone_number, 'EMAIL':email, 'TWITTER USERNAME':twitterUsername, 'CAR NUMBER':carNo}
         Customer.li = li2
 
     def save_CustomerInfo(self):                #writes customer information to file
         writer = open("HistoryLog.md", "a+")
         writer.write(str(Customer.li))
         writer.close()
+
+class Notification:
+    def send_Notification():
+        pass
+
 
 if __name__ == "__main__":
     newCustomer = Customer()
