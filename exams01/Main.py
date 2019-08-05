6a270a93ec82560720ecc0a58ef90c9ffd9da575
nathanielam0ah <nathanielam0ah@github.com>
2019-08-05 21:11:47 +0000

    Improved Menu

--- exams01/Main.py
+++ exams01/Main.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python
 
 from datetime import date, datetime
-import threading
+import getpass
 
 #importing local modules
 from Customer import Customer
+from Management import ManagementLog
 from CarWash import Wash
 import SendEmail
+import config
 
 class Main:
+    def __init__(self,password):
+        self.password = config.management_pass
+
     def get_menu(self):
         print("CAR WASH APPLICATION")
-        print("ENTER 1 FOR CUSOMER INFORMATION, 2 FOR START CAR WASH")
+        print("ENTER 1 FOR CUSOMER INFORMATION, 2 FOR START CAR WASH, 3 FOR ADMINISTRATOR LOG 4 TO EXIT MENU TERMINAL")
         try:
             menuInput = int(input(": "))
         except ValueError:
             print("INVALID INPUT")
+        while 1:
+            if menuInput != 4 :
+                testApp = Customer()
+                testApp.get_CustomerInfo()
+                testApp.save_CustomerInfo()
+            elif menuInput == 2:
+                testApp = Wash(False)
+                testApp.wash_car()
+                testApp.notify()
+            elif menuInput == 3:
+                try:
+                    print("ENTER MANAGEMENT PASSWORD")
+                    password = getpass.getpass("PASSWORD: ")
+                    if password == self.password:
+                        testApp = ManagementLog()
+                        testApp.readfile()
+                except:
+                    print("COULD NOT READ LOG FILE. YOU ARE NOT ADMIN")
+            elif menuInput == 4:
+                exit()
 
-        if menuInput == 1:
-            testApp = Customer()
-            testApp.get_CustomerInfo()
-            testApp.save_CustomerInfo()
-        elif menuInput == 2:
-            testApp = Wash(False)
-            testApp.wash_car()
-            testApp.notify()
+if __name__ == "__main__":
+    MainObject = Main(None)
+    MainObject.get_menu()
