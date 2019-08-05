32f66467399344091327cd1ec655f9b35c7028e6
nathanielam0ah <nathanielam0ah@github.com>
2019-08-05 20:34:51 +0000

    commented password variable input

--- exams01/SendEmail.py
+++ exams01/SendEmail.py
@@ -10,13 +10,11 @@ def send_email(subject,message,mailto):
         server=smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
-        password = getpass.getpass("PASSWORD: ")
-        server.login(config.email_server, password) #sender login
+        #password = getpass.getpass("PASSWORD: ")
+        server.login(config.email_server, config.email_server_pass) #sender login
         text="Subject: {}\n\n{}".format(subject,message)
         server.sendmail(config.email_client,mailto,text)
         server.quit()
         print(f"NOTIFICATION SENT TO {mailto}")
     except:
         print("FAILED TO SEND.")
-
-send_email("TEST EMAIL", "HELLO $USER", config.email_client)
