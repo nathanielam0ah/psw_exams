e3a146f6e287b284bf404d24997534ebe572243f
nathanielam0ah <nathanielam0ah@github.com>
2019-08-18 13:04:32 +0000

    imported sms in Main.

--- exams01/Main.py
+++ exams01/Main.py
@@ -7,15 +7,16 @@ import getpass
 from Customer import Customer
 from Management import ManagementLog
 from SendEmail import send_email
+from SendSms import send_sms
 from CarWash import Wash
-from config import email_client, email_server, email_server_pass, management_pass
+from config import email_server, email_server_pass, management_pass
 
-class Main:             #main menu class. that is obvious
+class Main:
     password = None
 
     def get_menu(self):             #where the attendant interacts with the application
         print("CAR WASH APPLICATION")
-        print("ENTER 1 FOR CUSTOMER INFORMATION, 2 FOR START CAR WASH, 3 FOR ADMINISTRATOR LOG 4 TO EXIT MENU TERMINAL")
+        print("ENTER 1 TO ADD TO CUSTOMER LOG, 2 FOR START CAR WASH, 3 FOR ADMINISTRATOR LOG 4 TO EXIT MENU TERMINAL")
         try:
             menuInput = int(input(": "))
         except ValueError:
@@ -25,6 +26,7 @@ class Main:             #main menu class. that is obvious
             testCustomer.get_customer_info()
             testCustomer.create_customer_database()
             testCustomer.save_customer_info()
+
         elif menuInput == 2:
             testWash = Wash(False)
             testWash.wash_car()
@@ -33,9 +35,13 @@ class Main:             #main menu class. that is obvious
                     message = "Hello {}, don't forget to pick up your car".format(Customer.li[0])
                     send_email("CAR WASH", message, Customer.li[2])
                 except:
-                    print("COULD NOT SEND EMAIL NOTIFICATION")
-            else:
-                print("FACING ISSUES CHECK IN LATER")
+                    print("ERRORxCUSOTOMER_NAMExEMAIL_EMPTY")
+                try:
+                    message = "Hello {}, don't forget to pick up your car".format(Customer.li[0])
+                    send_sms(message,Customer.li[1])
+                except:
+                    print("ERRORxCUSOTMER_NAMExSMS_EMPTY. ")
+            
         elif menuInput == 3:
             print("ENTER MANAGEMENT PASSWORD")
             password = getpass.getpass("PASSWORD: ")
@@ -47,10 +53,11 @@ class Main:             #main menu class. that is obvious
                     print("COULD NOT READ LOG FILE. NO LOG OR INCORRECT PASSWORD")
             else:
                 print("INCORRECT PASSWORD")
+
         elif menuInput == 4:
             exit()
 
-if __name__ == "__main__":
+if __name__ == "__main__": # where the Main class is instantiated
     MainObject = Main()
     while 1:
         MainObject.get_menu()
